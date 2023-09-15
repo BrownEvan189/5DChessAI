66e5c8d34c904192790a7056cee1bc88ac9c9b78
BrownEvan189 <123338562+BrownEvan189@users.noreply.github.com>
2023-09-14 20:24:23 -0500

    Update 5DChess.py

--- 5DChess.py
+++ 5DChess.py
@@ -1,28 +1,4 @@
-class Piece():
-    def __init__(self, name, coord):
-        self.name = name
-        self.coord = coord
 
-    def getMovements(self):
-        if self.name == "king":
-            return [0, 0, 1, 0]
-        
-def movePiece(board, piece):
-    move = piece.getMovements()
-    board[piece.coord[0] + move[0]][piece.coord[1] + move[1]][piece.coord[2] + move[2]][piece.coord[3] + move[3]] = piece
-    board[piece.coord[0]][piece.coord[1]][piece.coord[2]][piece.coord[3]] = ' '
-    piece.coord = [piece.coord[0] + move[0], piece.coord[1] + move[1], piece.coord[2] + move[2], piece.coord[3] + move[3]]
-        
-def move(board, copy, piece):
-    board[piece.coord[0]].append(copy)
-    movePiece(board, board[piece.coord[0]][piece.coord[1] + 1][piece.coord[2]][piece.coord[3]])
-
-
-board = [[[[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
-           [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
-           [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
-           [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
-           [' ', ' ', ' ', ' ', ' ', Piece("king", [0, 0, 4, 5]), ' ', ' '], 
            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]]]
